ec49557c0a9382db0a0a408bd4ff3a3eb4f59047
Cédric <60101615+ImAlsoATomato@users.noreply.github.com>
2020-03-02 14:10:23 +0100

    Added Structure date

--- timelib.h
+++ timelib.h
@@ -7,14 +7,20 @@
 
 #ifndef HEADER_FILE
 #define HEADER_FILE
+struct date{
+  int day;
+  int month;
+  int year;
+};
 
 
-int day_of_the_year(int day, int month, int year);
+
+int day_of_the_year(struct date);
 int is_leapyear(int year);
-int get_weekday(int day, int month, int year);
-int exists_date(int day, int month, int year);
+int get_weekday(struct date);
+int exists_date(struct date);
 int get_days_for_month(int month, int year);
-int get_weekday(int day, int month, int year);
-void input_date(int *day, int *month, int *year);
+int get_weekday(struct date);
+struct date input_date();
 
 #endif
